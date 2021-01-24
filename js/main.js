5b4e95673cad4830da7cec1a9ae3c44276d183a9
dzhelez2903 <dzhelez@yahoo.com>
2021-01-24 14:34:41 +0200

    header done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*set slider*/
+
+$(document).ready(function () {
+  //$('.header').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
