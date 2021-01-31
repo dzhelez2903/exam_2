98e5c73c132bfcc6afa449d38eb68dd2bd033999
dzhelez2903 <dzhelez@yahoo.com>
2021-01-31 19:33:27 +0200

    added media queries

--- js/main.js
+++ js/main.js
@@ -9,12 +9,6 @@ $('.second_sectoin_container').slick({
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 1354,
-      settings: {
-        arrows: false,
-      }
-    },
-    {
       breakpoint: 991,
       settings: {
         slidesToShow: 2,
@@ -22,10 +16,61 @@ $('.second_sectoin_container').slick({
       }
     },
     {
-      breakpoint: 767,
+      breakpoint: 768,
       settings: {
         slidesToShow: 1,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint:480,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+        dots: false,
       }
     }
   ]
+});
+
+/*set mobile menu*/
+
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find(
+    ".header__mobile_menu__button",
+    ".header__mobile_menu__lines"
+  );
+  let links = menu.find(".header__mobile_menu__link");
+  let overlay = menu.find(".header__mobile_menu__overlay");
+
+  button.on("click", (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on("click", () => toggleMenu());
+  overlay.on("click", () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass("header__mobile_menu_active");
+
+    if (menu.hasClass("header__mobile_menu_active")) {
+      $("body").css("overlow", "hidden");
+    } else {
+      $("body").css("overlow", "visible");
+    }
+  }
+}
+
+burgerMenu(".header__mobile_menu");
+
+/*lazy scroll*/
+
+$('a[href^="#"]').click(function () {
+  $('html, body').animate({
+      scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+  }, 500);
+
+  return false;
 });
